2ce58abe5b6e5b43111611617ba4b8bc3c1cb3d2
msontop <msontop.me@gmail.com>
2023-12-22 20:15:17 +0100

    ...

--- newscall/views.py
+++ newscall/views.py
@@ -1,31 +1,3 @@
-# import requests
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-
-# class NewsAPIView(APIView):
-#     def get(self, request):
-#         url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list"
-#         querystring = {"region":"US","snippetCount":"28"}
-#         payload = "Pass in the value of uuids field returned right in this endpoint to load the next page, or leave empty to load first page"
-#         headers = {
-#             "content-type": "text/plain",
-#             "X-RapidAPI-Key": "d1361dddfamshb19e08e63c9b86dp1fe0e4jsnf934221382e3",
-#             "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-#         }
-#         response = requests.post(url, data=payload, headers=headers, params=querystring)
-#         return Response(response.json())
-
-# class NewsDetail(APIView):
-#     def get(self, request, uuid):
-#         url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/get-details"
-#         querystring = {"uuid": uuid, "region": "US"}
-#         headers = {
-#             "X-RapidAPI-Key": "d1361dddfamshb19e08e63c9b86dp1fe0e4jsnf934221382e3",
-#             "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-#         }
-#         response = requests.get(url, headers=headers, params=querystring)
-#         return Response(response.json())
-
 from django.core.cache import cache
 from datetime import timedelta
 from rest_framework.views import APIView
