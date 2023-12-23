d9913334ab99ac099a58b8d59386ff37d760318b
msontop <msontop.me@gmail.com>
2023-12-23 03:33:28 +0100

    ...

--- yahoofinapi/urls.py
+++ yahoofinapi/urls.py
@@ -1,5 +1,10 @@
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('newscall.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
